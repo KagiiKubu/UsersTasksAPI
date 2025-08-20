d68ef80db2be5900930653cb1a954d0470e6acef
kkubu023 <kkubu023@student.wethinkcode.co.za>
2025-08-20 13:27:33 +0200

    Added Task Repository Interface

---
+++ Repositories/ITaskRepository.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersTasksAPI.Models;
+
+
+namespace UsersTasksAPI.Repositories
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<UserTask>> GetAllTasks();
+
+        Task<UserTask> GetTaskByTitle(string title);
+        Task<bool> AddNewTask(UserTask newTask);
+        Task<bool> UpdateTask(UserTask task);
+        Task<bool> DeleteTaskByTitle(string title);
+
+        Task<IEnumerable<UserTask>> GetExpiredTasks();
+        Task<IEnumerable<UserTask>> GetActiveTasks();
+        Task<IEnumerable<UserTask>> GetTasksByDate(DateOnly givenDate);
+        Task<IEnumerable<UserTask>> GetTasksByAssignee(int assigneeId);
+    }
+}
